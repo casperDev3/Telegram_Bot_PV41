04c7e83c5128c5fb6d798d3f50d238bc02f02a73
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2025-02-15 16:07:21 -0800

    add first template

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+import asyncio
+import logging
+import sys
+from os import getenv
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = "7331456101:AAHmQ0Sf6NqQCEjfIC57uWgaR_umLIKzF2U"
+
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+# reply keyboard markup
+def get_main_keyboard():
+    return {
+        "keyboard": [
+            [{"text": "⚙️Налаштування"}, {"text": "🏚Про нас"}],
+            [{"text": "📞Зв'язатися з нами"}]
+        ],
+        "resize_keyboard": False
+    }
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+    await message.answer("Мур-мур-мур", reply_markup=get_main_keyboard())
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    try:
+        msg = message.text
+        if msg == "⚙️Налаштування":
+            await message.answer("Виберіть налаштування")
+        elif msg == "🏚Про нас":
+            await message.answer("Ми найкращий бот для вас")
+        elif msg == "📞Зв'язатися з нами":
+            await message.answer("Телефонуйте нам за номером +380123456789")
+        else:
+            await message.send_copy(chat_id=message.chat.id)
+
+    except TypeError:
+        await message.answer("Nice try!")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
